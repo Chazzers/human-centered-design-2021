f09fd68c2383e87f7e3b2c5c8c2e45b05b8cd682
Chazzers <https://ChazzM@bitbucket.org>
2021-04-22 15:31:21 +0200

    add keyboard inputs to demo

--- js/main.js
+++ js/main.js
@@ -10,48 +10,80 @@ const fromChoice = document.getElementById('from-choice')
 const toChoice = document.getElementById('to-choice')
 const formTime = document.getElementById('time')
 const formDate = document.getElementById('date')
+const reisAssistentieAanvraagButton = document.querySelector('#option-content header a')
 
 Date.prototype.addHours = function(h) {
 	this.setTime(this.getTime() + (h*60*60*1000));
 	return this;
 }
 
-nietVanWerk.addEventListener('click', () => {
-	naarWerkContainer.style.display = 'none'
-	waarNaarToeContainer.style.display = 'block'
-})
-
-form.addEventListener('submit', (e) => {
-	e.preventDefault()
-	const fromValue = fromChoice.value
-	const toValue = toChoice.value
-	const timeValue = formTime.value
-	const dateValue = formDate.value
-	const when = new Date(`${dateValue} ${timeValue}`)
-
-	console.log(when)
-
-	const searchParams = new URLSearchParams({
-		vertrek: fromValue,
-		aankomst: toValue,
-		type: 'vertrek',
-		tijd: when
+if(nietVanWerk) {
+	nietVanWerk.addEventListener('click', () => {
+		naarWerkContainer.style.display = 'none'
+		waarNaarToeContainer.style.display = 'block'
 	})
-	const url = `https://www.ns.nl/reisplanner/#/?${searchParams}`
+	
+	form.addEventListener('submit', (e) => {
+		e.preventDefault()
+		const fromValue = fromChoice.value
+		const toValue = toChoice.value
+		const timeValue = formTime.value
+		const dateValue = formDate.value
+		const when = new Date(`${dateValue} ${timeValue}`)
+	
+		console.log(when)
+	
+		const searchParams = new URLSearchParams({
+			vertrek: fromValue,
+			aankomst: toValue,
+			type: 'vertrek',
+			tijd: when
+		})
+		const url = `https://www.ns.nl/reisplanner/#/?${searchParams}`
+	
+		window.location.href = url;
+	})
+	
+	const today = new Date()
+	const tomorrow = new Date(today)
+	
+	today.addHours(1)
+	tomorrow.setDate(tomorrow.getDate() + 1)
+	tomorrow.setHours(8)
+	tomorrow.setMinutes(0)
+	tomorrow.setMilliseconds(0)
+	const tomorrowHref = new Date(tomorrow.getTime() - new Date().getTimezoneOffset() * 60 * 1000).toISOString()
+	const todayHref = new Date(today.getTime() - new Date().getTimezoneOffset() * 60 * 1000).toISOString()
+}
 
-	window.location.href = url;
+
+const allTravelOptions = document.querySelectorAll('.ov-optie')
+if(allTravelOptions) {
+	allTravelOptions[0].focus()
+}
+let index = 0
+
+document.addEventListener('keydown', event => {
+	event.preventDefault()
+	if(event.key === 'ArrowUp') {
+		index -= 1
+	}
+	if(event.key === 'ArrowDown') {
+		index += 1
+	}
+	if(index < 0) {
+		index = allTravelOptions.length - 1
+	}
+	if(index > allTravelOptions.length - 1){
+		index = 0;
+	}
+	if(event.key === 'ArrowUp' || 'ArrowDown' || 'ArrowLeft') {
+		allTravelOptions[index].focus()
+	}
+	if(event.key === 'ArrowRight') {
+		reisAssistentieAanvraagButton.focus()
+	}
 })
-
-const today = new Date()
-const tomorrow = new Date(today)
-
-today.addHours(1)
-tomorrow.setDate(tomorrow.getDate() + 1)
-tomorrow.setHours(8)
-tomorrow.setMinutes(0)
-tomorrow.setMilliseconds(0)
-const tomorrowHref = new Date(tomorrow.getTime() - new Date().getTimezoneOffset() * 60 * 1000).toISOString()
-const todayHref = new Date(today.getTime() - new Date().getTimezoneOffset() * 60 * 1000).toISOString()
 
 // naarWerkLink.href = `https://www.ns.nl/reisplanner/#/?vertrek=Eindhoven%20Centraal&vertrektype=treinstation&aankomst=Amsterdam%20Centraal&aankomsttype=treinstation&type=vertrek&tijd=${tomorrowHref}&toegankelijk=true`
 // vanWerkLink.href= `https://www.ns.nl/reisplanner/#/?vertrek=Eindhoven%20Centraal&vertrektype=treinstation&aankomst=Amsterdam%20Centraal&aankomsttype=treinstation&type=vertrek&tijd=${todayHref}&toegankelijk=true`
